a9e70410e2ad1bfd650627f530ae0ce81205b5ab
Sparrow <vlad22832006@gmail.com>
2025-01-13 18:49:09 +0300

    fix2

--- i_pkgs/i_pkgs.sh
+++ i_pkgs/i_pkgs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
+mkdir /etc/systemd/system-preset
 ln -s /dev/null /etc/systemd/system-preset/99-default.preset
 
-pkgs=$(cat ./pkgs.txt)
+pkgs=$(cat i_pkgs/pkgs.txt)
 
 IFS=" " read -ra pkgs <<< "$pkgs"
-
 for pkg in "${pkgs[@]}"; do 
   pacman -Sy "$pkg" --noconfirm
 done
