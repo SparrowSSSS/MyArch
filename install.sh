1f71c25a43f6ad6992a39a33838117522bf7249f
Sparrow <vlad22832006@gmail.com>
2025-01-15 14:18:40 +0300

    fix12

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 export DIR=/skel
 
-waybar_dir=".config/waybar"
+waybar_dir="~/.config/waybar"
 
 cp -r aurrepo /usr/local/share
 
@@ -14,8 +14,8 @@ cp -r system/. /etc/systemd/system
 
 cp -r usr/. /usr
 
-chmod +x scripts/* $waybar_dir/Waybar-3.0/playerctl.sh $waybar_dir/scripts/*
-
 cp -r .config .themes scripts ~/
+
+chmod +x ~/scripts/* $waybar_dir/Waybar-3.0/playerctl.sh $waybar_dir/scripts/*
 
 mkinitcpio -P
